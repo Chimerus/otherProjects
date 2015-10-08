bcf46ac906314cbd3a0b3b0c5a4f30de52906389
Chimerus <jlc370@gmail.com>
2015-10-08 10:45:16 -0400

    Added background variants and specialties

--- DnDcharacterGenerator.js
+++ DnDcharacterGenerator.js
@@ -133,33 +133,147 @@ switch(Math.floor((Math.random() * 13)+1)){
   break;
   case 3:
   character.Background = "Criminal";
+ var roll=Math.floor((Math.random() * 8)+1);
+    if (roll==1){
+        character.Background += "- Blackmailer";
+      } else if (roll == 2){
+        character.Background += "- Burglar";
+      } else if (roll == 3){
+        character.Background += "- Enforcer";
+      } else if (roll == 4){
+        character.Background += "- Fence";
+      } else if (roll == 5) {
+        character.Background += "- Highway Robber";
+      } else if (roll == 6) {
+        character.Background += "- Hired Killer";
+      } else if (roll == 7) {
+        character.Background += "- Pickpocket";
+      } else {
+        character.Background += "- Smuggler";
+      }
   break;
   case 4:
-  character.Background = "Entertainer";
+      if (Math.floor((Math.random() * 2)+1)==1){
+        character.Background = "Entertainer";   
+      var roll=Math.floor((Math.random() * 10)+1);
+      if (roll==1){
+        character.Background += "- Actor";
+      } else if (roll == 2){
+        character.Background += "- Dancer";
+      } else if (roll == 3){
+        character.Background += "- Fire-eater";
+      } else if (roll == 4){
+        character.Background += "- Jester";
+      } else if (roll == 5) {
+        character.Background += "- Juggler";
+      } else if (roll == 6) {
+        character.Background += "- Instrumentalist";
+      } else if (roll == 7) {
+        character.Background += "- Poet";
+      } else if (roll == 8) {
+        character.Background += "- Singer";
+      } else if (roll == 9) {
+        character.Background += "- Storyteller";
+      } else {
+        character.Background += "- Tumbler";
+        }
+      } else {
+        character.Background = "Gladiator ";
+      }
   break;
   case 5:
   character.Background = "Folk Hero";
   break;
   case 6:
-  character.Background = "Guild Artisan";
+  if (Math.floor((Math.random() * 2)+1)==1){
+        character.Background = "Guild Artisan";
+      } else {
+         character.Background = "Guild Merchant";
+      }
   break;
   case 7:
-  character.Background = "Hermit";
+    character.Background = "Hermit";
   break;
   case 8:
-  character.Background = "Noble";
+    if (Math.floor((Math.random() * 2)+1)==1){
+        character.Background = "Noble";
+      } else {
+        character.Background = "Knight";
+      }
   break;
   case 9:
-  character.Background = "Outlander";
+    character.Background = "Outlander";
+    var roll=Math.floor((Math.random() * 10)+1);
+      if (roll==1){
+        character.Background += "- Forester";
+      } else if (roll == 2){
+        character.Background += "- Trapper";
+      } else if (roll == 3){
+        character.Background += "- Homesteader";
+      } else if (roll == 4){
+        character.Background += "- Guide";
+      } else if (roll == 5) {
+        character.Background += "- Exile/Outcast";
+      } else if (roll == 6) {
+        character.Background += "- Bounty Hunter";
+      } else if (roll == 7) {
+        character.Background += "- Pilgrim";
+      } else if (roll == 8) {
+        character.Background += "- Tribal Nomad";
+      } else if (roll == 9) {
+        character.Background += "- Hunter-Gatherer";
+      } else {
+        character.Background += "- Tribal Marauder";
+        }
   break;  
   case 10:
   character.Background = "Sage";
+   var roll=Math.floor((Math.random() * 8)+1);
+      if (roll==1){
+        character.Background += "- Alchemist";
+      } else if (roll == 2){
+        character.Background += "- Astronomer";
+      } else if (roll == 3){
+        character.Background += "- Discredited Academic";
+      } else if (roll == 4){
+        character.Background += "- Librarian";
+      } else if (roll == 5) {
+        character.Background += "- Professor";
+      } else if (roll == 6) {
+        character.Background += "- Researcher";
+      } else if (roll == 7) {
+        character.Background += "- Wizard's Apprentice";
+      } else {
+        character.Background += "- Scribe";
+      }
   break;
   case 11:
-  character.Background = "Sailor";
+    if (Math.floor((Math.random() * 2)+1)==1){
+        character.Background = "Sailor";
+      } else {
+         character.Background = "Pirate";
+      }
   break;
   case 12:
   character.Background = "Soldier";
+     var roll=Math.floor((Math.random() * 8)+1);
+      if (roll==1){
+        character.Background += "- Officer";
+      } else if (roll == 2){
+        character.Background += "- Scout";
+      } else if (roll == 3){
+        character.Background += "- Infantry";
+      } else if (roll == 4){
+        character.Background += "- Calvary";
+      } else if (roll == 5) {
+        character.Background += "- Healer";
+      } else if (roll == 6) {
+        character.Background += "- Quartermaster";
+      } else if (roll == 7) {
+        character.Background += "- Standard Bearer";
+      } else {
+        character.Background += "- Support Staff (cook,blacksmith,etc.)";
+      }
   break;
   case 13:
   character.Background = "Urchin";
