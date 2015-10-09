e03b1b32b06bc64d0eca05d35d1e12f20eaf40b0
Chimerus <jlc370@gmail.com>
2015-10-09 14:42:01 -0400

    Stat Block Generator

---
+++ statBlockGenerator.js
@@ -0,0 +1,24 @@
+function statGen() {
+  //generate a stat block by rolling 4d6
+  //adding highest 3 together, keeping that number
+  //doing that 6 times
+var array = [];
+for(var loop =0; loop<6; loop++){
+    var stat = [];
+//to get the 4d6 roll
+    for(var idx=0; idx<4; idx++){
+    stat.push(Math.floor((Math.random() * 6)+1));
+    }
+  stat.sort();
+//drop the lowest
+  stat.splice(0,1);
+//add them together
+  var total=0;
+  for (idx=0; idx<3; idx++){
+  total +=stat[idx];
+  }
+  //add it to final array
+  array.push(total);
+}
+return array;
+}
